7ff27ee82894970e28dcca07e897c33fc7d17145
Aditya Singh <aditya.singh@criodo.com>
2024-03-07 01:03:01 +0530

    Error Removed in Project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -35,7 +35,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/my-hotels", myHotelsRoutes);
 app.use("/api/hotels", hotelRoutes);
-app.use("/api/my-bookings", hotelRoutes);
+app.use("/api/my-bookings", bookingRoutes);
 
 app.get("*", (req:Request, res:Response)=>{
     res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'))
